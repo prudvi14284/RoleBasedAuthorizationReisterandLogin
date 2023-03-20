5fb5855d885d864f1cde0042b371fed7b17531b9
yedduprudvi@outlook.com <yedduprudvi@outlook.com>
2023-03-20 15:46:02 +0530

    Add new Code

---
+++ RoleBasedAuthorization/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoleBasedAuthorization.Controllers
+{
+    public class UserAuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
