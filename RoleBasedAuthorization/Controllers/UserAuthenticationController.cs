e3f359691b546c6b8056228b654567fed1abee05
yedduprudvi@outlook.com <yedduprudvi@outlook.com>
2023-03-21 11:00:40 +0530

    Add new code

--- RoleBasedAuthorization/Controllers/UserAuthenticationController.cs
+++ RoleBasedAuthorization/Controllers/UserAuthenticationController.cs
@@ -1,12 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RoleBasedAuthorization.Models.DTO;
+using RoleBasedAuthorization.Repositories.Abstract;
 
 namespace RoleBasedAuthorization.Controllers
 {
     public class UserAuthenticationController : Controller
     {
+        private readonly IUserAuthenticationService _service;
+
+        public UserAuthenticationController(IUserAuthenticationService service)
+        {
+            this._service = service;
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        //Registration Post
+        [HttpPost]
+        public async Task<IActionResult> Registration(RegistrationModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            model.Role = "user";
+            var result = await _service.RegistrationAsync(model);
+            TempData["msg"] = result.Message;
+            return RedirectToAction(nameof(Registration));
+        }
+
         public IActionResult Login()
         {
             return View();
+        }
+
+        // Login post
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = await _service.LoginAsync(model);
+            if(result.StatusCode == 1)
+            {
+                return RedirectToAction("Display", "Dashboard");
+            }
+            else
+            {
+                TempData["msg"] = result.Message;
+                return RedirectToAction(nameof(Login));
+            }
+        }
+
+        
+        [Authorize]
+        public async Task Logout()
+        {
+            await _service.LogoutAsync();
         }
     }
 }
